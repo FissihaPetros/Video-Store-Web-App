1df1403a87df244fdeacd355ebec9f351f6eaf55
Fissiha Petros <denjeman12@gmail.com>
2021-10-13 11:00:20 +0300

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Video_Store_Web_App.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Video_Store_Web_App.Startup))]
+namespace Video_Store_Web_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
